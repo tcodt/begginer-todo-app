4b5dd2a87de6b93da2be169f5b869ae3c4f81850
TCOD <tcoddev@gmail.com>
2024-06-16 22:06:38 +0330

    initial project from scratch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1>TODO LIST WITH REACT REDUX</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
