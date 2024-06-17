5434caaf288a13735c74e5bb1d54c1948fab7fde
TCOD <tcoddev@gmail.com>
2024-06-17 17:41:45 +0330

    add logic and using redux

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,34 +1,66 @@
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo, toggleTodo } from "../redux/todoSlice";
+
 export default function TodoList() {
+  const [newTodo, setNewTodo] = useState("");
+  const inputRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const allTodos = useSelector((state) => state.todos.todos);
+
+  const addTodoHandler = () => {
+    if (newTodo.trim() !== "") {
+      dispatch(
+        addTodo({ id: allTodos.length + 1, title: newTodo, isCompleted: false })
+      );
+      setNewTodo("");
+      inputRef.current.focus();
+    }
+  };
+
+  const handleToggleTodo = (todoID) => {
+    dispatch(toggleTodo(todoID));
+  };
+
+  const handleDelete = (todoID) => {
+    dispatch(deleteTodo(todoID));
+  };
+
   return (
     <div className="todo-container">
       <h1>TCOD TODO LIST</h1>
       <div className="input-box">
-        <input type="text" placeholder="Add a todo" />
-        <button>Add</button>
+        <input
+          type="text"
+          placeholder="Add a todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          ref={inputRef}
+        />
+        <button onClick={addTodoHandler}>Add</button>
       </div>
       <div className="todo-box">
         <ul>
-          <li>
-            First todo{" "}
-            <div className="todo-options">
-              <input type="checkbox" />
-              <button>Delete</button>
-            </div>
-          </li>
-          <li>
-            Second todo{" "}
-            <div className="todo-options">
-              <input type="checkbox" />
-              <button>Delete</button>
-            </div>
-          </li>
-          <li>
-            Third todo{" "}
-            <div className="todo-options">
-              <input type="checkbox" />
-              <button>Delete</button>
-            </div>
-          </li>
+          {allTodos.map((todo) => (
+            <li key={todo.id}>
+              <span
+                style={{
+                  textDecoration: todo.isCompleted ? "line-through" : "none",
+                }}
+              >
+                {todo.title}
+              </span>
+              <div className="todo-options">
+                <input
+                  type="checkbox"
+                  checked={todo.isCompleted}
+                  onChange={() => handleToggleTodo(todo.id)}
+                />
+                <button onClick={() => handleDelete(todo.id)}>Delete</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -6,10 +6,18 @@ const todoSlice = createSlice({
     todos: [],
   },
   reducers: {
-    addTodo: () => {},
-    toggleTodo: () => {},
-    deleteTodo: () => {},
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      todo.isCompleted = !todo.isCompleted;
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
   },
 });
 
+export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
 export default todoSlice.reducer;
